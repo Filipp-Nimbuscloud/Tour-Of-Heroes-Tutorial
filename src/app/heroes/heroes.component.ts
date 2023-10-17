31918f688e5214efe59ed20951b680826f34f8e8
Philipp <philipp.haderer@nimbuscloud.at>
2023-10-17 14:55:41 +0200

    Finished up services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from "../message.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
-  heroes: Hero[] = [];
-  constructor(private heroService: HeroService) {};
-  protected readonly onselect = onselect;
+export class HeroesComponent implements OnInit{
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService: MessageService) {};
   ngOnInit(): void {
     this.getHeroes();
   }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
     this.heroService.getHeroes()
