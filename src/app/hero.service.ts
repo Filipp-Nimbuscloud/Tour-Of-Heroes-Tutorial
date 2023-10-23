b406b6c9d47a4832fb417db5bfc33e8165ef1442
Philipp <philipp.haderer@nimbuscloud.at>
2023-10-23 16:06:33 +0200

    resolved conflicts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,8 +7,10 @@ import { MessageService } from "./message.service";
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
   constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,14 +11,18 @@ import { MessageService } from "../message.service";
 export class HeroesComponent implements OnInit{
   selectedHero?: Hero;
   heroes: Hero[] = [];
+
   constructor(private heroService: HeroService, private messageService: MessageService) {};
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
